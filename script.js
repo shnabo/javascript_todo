32a43c8385608298b8d2f0471a318a0f431db900
shnabo <s.villella@live.com>
2019-11-29 11:52:51 -0500

    Created function to show whether a todo has been completed or not

--- script.js
+++ script.js
@@ -100,7 +100,21 @@ var handlers = {
 var view = {
   displayTodos: function () {
     var todosUl = document.querySelector('ul');
-    var todoLi = document.createElement('li');
-    todosUl.appendChild(todoLi);
+    todosUl.innerHTML = "";
+
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+      
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
+      }
+
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
   }
 };
