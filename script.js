01d2b9cc20f82ebb2c270a690f02fc46a2645f58
shnabo <s.villella@live.com>
2020-01-03 14:58:58 -0500

    add a function to create deleteButton

--- script.js
+++ script.js
@@ -96,6 +96,12 @@ var view = {
 
       todoLi.textContent = todoTextWithCompletion;
       todosUl.appendChild(todoLi);
+     }
+    },
+createDeleteButton: function() {
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'deleteButton';
+      return deleteButton;
     }
-  }
 };
